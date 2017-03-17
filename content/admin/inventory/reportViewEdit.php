e98c2f1355b44628da716b5b318c9e1c7508e7ac
root <work@tier5.us>
2017-03-17 14:55:46 +0530

    Add Location Edit--correction v1.1

--- content/admin/inventory/reportViewEdit.php
+++ content/admin/inventory/reportViewEdit.php
@@ -209,9 +209,9 @@ if (count($data_color) > 0) {
     {
         for ($l = 0; $l < count($data_inv); $l++) 
         {
-            if (isset($data_inv[$l]['quantity']) && $data_inv[$l]['quantity'] != '')
+            if (isset($data_inv[$l]['st_quantity']) && $data_inv[$l]['st_quantity'] != '')
             {
-                $data_inv[$l]['quantity'] = $data_inv[$l]['quantity'];
+                $data_inv[$l]['quantity'] = $data_inv[$l]['st_quantity'];
             }
         }
     }
