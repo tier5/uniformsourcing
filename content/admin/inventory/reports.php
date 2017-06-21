d00828b2084bd738a8d81fe5b6ac2a55e1be6541
Tier5 LLC <github@tier5.us>
2017-06-21 18:26:21 +0530

    Inventory V2.9.7 stable released to dev

--- content/admin/inventory/reports.php
+++ content/admin/inventory/reports.php
@@ -715,7 +715,9 @@ else
 <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/selectize.js/0.12.4/js/standalone/selectize.min.js"></script>
 <script>
-    $('#box_num').selectize();
+    $('#box_num').selectize({
+        maxOptions: <?php echo count($data_storage); ?>
+    });
 </script>
 <?php  require('../../trailer.php');
 ?>
