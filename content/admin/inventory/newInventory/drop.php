a846bc23eff8e1a660db308185da02a5c9b8908d
Tier5 LLC <github@tier5.us>
2017-07-12 18:33:15 +0530

    restructure V1.0.1

---
+++ content/admin/inventory/newInventory/drop.php
@@ -0,0 +1,22 @@
+<?php
+ini_set('max_execution_time', 30000000);
+set_time_limit(0);
+ini_set('memory_limit', '2048M');
+require('Application.php');
+$sql = '';
+$sql = 'DROP TABLE "tbl_invQuantity"';
+if (!($resultProduct = pg_query($connection, $sql))) {
+    print("Failed invQuery: " . pg_last_error($connection));
+    exit;
+}
+pg_free_result($resultProduct);
+$sql = '';
+$sql = 'DROP TABLE "tbl_invUnit"';
+if (!($resultProduct = pg_query($connection, $sql))) {
+    print("Failed invQuery: " . pg_last_error($connection));
+    exit;
+}
+pg_free_result($resultProduct);
+echo "done";
+exit;
+?>
